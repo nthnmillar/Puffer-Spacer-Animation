39e4ba55e2b6aa953a49e20b7d51108aa4ec3392
Nathan Ryan Millar <nthnmillar@gmail.com>
2018-01-23 16:31:09 +1100

    animationSprite
    Got it to wok

---
+++ scripts/script.js
@@ -0,0 +1,27 @@
+
+
+$('.puff').animateSprite({
+
+    fps:10,
+    animations:{
+      puff:[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+    },
+    loop:true,
+    autoplay:true,
+    complete:function(){
+      alert("animation end");
+    }
+});
+
+$('.spacer').animateSprite({
+
+    fps:10,
+    animations:{
+      puff:[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28]
+    },
+    loop:true,
+    autoplay:true,
+    complete:function(){
+      alert("animation end");
+    }
+});
