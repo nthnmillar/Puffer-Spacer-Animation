d8186c1dd4cc7dad9c7a021d7c17c94fd0bf070d
Nathan Ryan Millar <nthnmillar@gmail.com>
2018-01-24 15:30:31 +1100

    Everything works
    The animations start when the buttons are clicked on

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 $('#puff').animateSprite({
     fps:10,
     animations:{
-      puff:[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+      puff:[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
     },
     loop:false,
     autoplay:false,
