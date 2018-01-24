c5fb2a439ddfce8cda8679e93042f93634896930
Nathan Ryan Millar <nthnmillar@gmail.com>
2018-01-24 14:44:07 +1100

    Breathing now works onclick

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,31 @@
-
-
-$('.puff').animateSprite({
-
+$('#puff').animateSprite({
     fps:10,
     animations:{
       puff:[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
     },
-    loop:true,
-    autoplay:true,
+    loop:false,
+    autoplay:false,
     complete:function(){
-      alert("animation end");
+    //  alert("animation end");
     }
 });
 
-$('.spacer').animateSprite({
+$('#button-puff').click(function(){
+    $("#puff").animateSprite('restart');
+});
 
+$('#spacer').animateSprite({
     fps:10,
     animations:{
       puff:[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28]
     },
-    loop:true,
-    autoplay:true,
+    loop:false,
+    autoplay:false,
     complete:function(){
-      alert("animation end");
+    //  alert("animation end");
     }
+});
+
+$('#button-space').click(function(){
+    $("#spacer").animateSprite('restart');
 });
